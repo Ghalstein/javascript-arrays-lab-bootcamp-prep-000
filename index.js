97bc5150fdfae0a25ba37675e4bce2a222373e03
Garrett Halstein <garrett.halstein@emory.edu>
2019-01-18 06:50:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 var kittens = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendKitten(kitten){
-  kittens.push(kittens);
+function destructivelyAppendKitten(name){
+  kittens.push(name);
   return kittens;
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+function appendKitten(name){
+  newKittens = [...kittens];
+  newKittens.push(name);
+  return newKittens;
+}
+function prependKitten(name){
+  newKittens = [...kittens];
+  newKittens.unshift(name);
+  return newKittens
 }
